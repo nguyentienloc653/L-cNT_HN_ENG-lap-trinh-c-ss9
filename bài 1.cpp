6ac539c1a203ed395b3d866c1620a04c453a7dc6
nguyentienloc653 <locnguyentien653@gmail.com>
2024-12-02 02:04:03 +0700

    upload ss9-1

---
+++ bài 1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int n, currentLength;
+    printf("Enter amount of element: ");
+    scanf("%d", &n);
+    if (n > 100 || n < 0){
+        printf("Invalid element");
+        return 0;
+    }
+    currentLength = n;
+    printf("Enter element:\n");
+    for (int i = 0; i < n; i++){
+        printf("[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+    int addValue, addIndex;
+    printf("Add element: ");
+    scanf("%d", &addValue);
+    printf("Enter add position: ");
+    scanf("%d", &addIndex);
+    if (addIndex < 0 || addIndex >= 100){
+        printf("Invalid posion\n");
+        return 0;
+    }
+    if (addIndex >= currentLength){
+        arr[addIndex] = addValue;
+        currentLength = addIndex + 1;
+    } else {
+        //chen phan tu moi
+        for (int i = currentLength; i > addIndex; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[addIndex] = addValue;
+        currentLength++;  //tang so luong phan tu hien tai
+    }
+    printf("After adding: ");
+    for (int i = 0; i < currentLength; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
